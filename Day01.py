9a54c478dd1abffa1881e4f7ae49b89edaee0e54
LHW9054 <lho90554@gmail.com>
2023-09-23 22:58:51 +0900

    파이썬 공부 첫날

---
+++ Day01.py
@@ -0,0 +1,52 @@
+# 기본 자료형
+# 정수형
+my_integer = 10
+
+# 실수형
+my_float = 3.14
+
+# 문자열
+my_string = "Hello, World!"
+
+# 불린 (참 또는 거짓)
+my_boolean = True
+
+# 연산자
+# 덧셈
+result_add = my_integer + my_float
+
+# 뺄셈
+result_sub = my_integer - my_float
+
+# 곱셈
+result_mul = my_integer * my_float
+
+# 나눗셈
+result_div = my_integer / my_float
+
+# 정수 나눗셈 (나머지 제외)
+result_floor_div = my_integer // my_float
+
+# 나머지 연산
+result_mod = my_integer % 3
+
+# 거듭제곱
+result_power = my_integer ** 2
+
+# 문자열 연결
+concatenated_string = my_string + " Have a nice day!"
+
+# 출력문
+print("정수형:", my_integer)
+print("실수형:", my_float)
+print("문자열:", my_string)
+print("불린:", my_boolean)
+
+print("덧셈 결과:", result_add)
+print("뺄셈 결과:", result_sub)
+print("곱셈 결과:", result_mul)
+print("나눗셈 결과:", result_div)
+print("정수 나눗셈 결과:", result_floor_div)
+print("나머지 연산 결과:", result_mod)
+print("거듭제곱 결과:", result_power)
+print("문자열 연결 결과:", concatenated_string)
